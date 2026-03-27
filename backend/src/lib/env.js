47426b637861a947c9d2590cdebfd861a4c924eb
Tony <kounleadissa@gmail.com>
2026-03-27 05:01:38 +0100

    feat: add any function in env.js

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,10 +1,20 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
-module.exports = {
-  ENV: {
-    MONGO_URI: process.env.MONGO_URI,
-    PORT: process.env.PORT || 3000,
-    NODE_ENV: process.env.NODE_ENV || "development"
-  }
+const ENV = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+  CLIENT_URL: process.env.CLIENT_URL,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  ARCJET_KEY: process.env.ARCJET_KEY,
+  ARCJET_ENV: process.env.ARCJET_ENV,
 };
+
+module.exports = { ENV };
