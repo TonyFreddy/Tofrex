89db80a67a195d32ad35b6cd4e671642b040328c
Tony <kounleadissa@gmail.com>
2026-03-27 05:04:54 +0100

    feat: add JWT token generation utility in utils.js

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const { ENV } = require("./env");
+
+const generateToken = (userId, res) => {
+  if (!ENV.JWT_SECRET) throw new Error("JWT_SECRET is not configured");
+
+  const token = jwt.sign({ userId }, ENV.JWT_SECRET, { expiresIn: "7d" });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: ENV.NODE_ENV !== "development",
+  });
+
+  return token;
+};
+
+module.exports = { generateToken };
