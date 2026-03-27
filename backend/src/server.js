0ddaf891f514e9084b281ac9a0a5f9c4072070c9
Tony <kounleadissa@gmail.com>
2026-03-27 02:18:12 +0100

    fix: switch to commonjs and resolve dotenv env variables issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,22 @@
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 const app = express();
 
-app.get("/api/auth/signup", (req , res) =>{
+console.log(process.env.PORT);
+
+app.get("/api/auth/signup", (req, res) => {
     res.send("Signup endpoint");
-} );
+});
 
-app.get("/api/auth/login", (req , res) =>{
+app.get("/api/auth/login", (req, res) => {
     res.send("Login endpoint");
-} );
+});
 
-app.get("/api/auth/logout", (req , res) =>{
+app.get("/api/auth/logout", (req, res) => {
     res.send("Logout endpoint");
-} );
+});
 
-app.listen(3000 , ()=> console.log('Server running on port 3000 '));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
