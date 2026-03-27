36ef7cd967abb7fab39949e59810949b531cff95
Tony <kounleadissa@gmail.com>
2026-03-28 00:51:45 +0100

    chore: install dependencies  arcjet, nodemailer

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { connectDB } = require("./lib/db");
-const path = require('path');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/auth.route");
 const messageRoutes = require("./routes/message.route");
+const { connectDB } = require("./lib/db");
+const { ENV } = require("./lib/env");
+const { app, server } = require("./lib/socket");
 
-dotenv.config();
+const PORT = ENV.PORT || 5000;
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json({ limit: "5mb" }));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    }); 
-}
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server running on port:", PORT);
   connectDB();
 });
+
